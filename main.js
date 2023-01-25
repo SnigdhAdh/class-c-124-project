9c2c6c686d6717bf71d8713815394ce6fd547e08
SnigdhAdh <111344743+SnigdhAdh@users.noreply.github.com>
2023-01-25 11:54:35 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,550);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    
+    
+}
+
+function draw(){
+    background('#969A97');
+}
